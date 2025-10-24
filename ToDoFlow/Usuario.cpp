5e1804783efa2b2bc118f2f49f72dde4b198b7ef
Randy Gutierrez <ran2511dy@gmail.com>
2025-10-24 16:49:13 -0600

    Nuevos cambios a usuario.cpp y .h

--- ToDoFlow/Usuario.cpp
+++ ToDoFlow/Usuario.cpp
@@ -1,1 +1,26 @@
-
+#pragma once
+#ifndef USUARIO_H
+#define USUARIO_H
+
+#include <string>
+
+class Usuario {
+private:
+    int id;
+    std::string nombre;
+    std::string email;
+
+public:
+    Usuario(int id, const std::string& nombre, const std::string& email);
+
+    int getId() const;
+    const std::string& getNombre() const;
+    const std::string& getEmail() const;
+
+    void setNombre(const std::string& nuevoNombre);
+    void setEmail(const std::string& nuevoEmail);
+
+    void imprimir() const;
+};
+
+#endif
